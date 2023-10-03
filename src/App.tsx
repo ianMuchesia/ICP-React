32d14f45bda0480083ba2b8f43385299f5523239
ianMuchesia <ianmuchesia@yahoo.com>
2023-10-03 08:13:10 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { backend } from './declarations/backend';
+
+
+function App() {
+  const [count, setCount] = useState<number | undefined>();
+  const [loading, setLoading] = useState(false);
+
+  // Get the current counter value
+  const fetchCount = async () => {
+    try {
+      setLoading(true);
+      const count = await backend.get();
+      console.log(count)
+      setCount(+count.toString()); // Convert BigInt to number
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const increment = async () => {
+    if (loading) return; // Cancel if waiting for a new count
+    try {
+      setLoading(true);
+      await backend.inc(); // Increment the count by 1
+      await fetchCount(); // Fetch the new count
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Fetch the count on page load
+  useEffect(() => {
+    fetchCount();
+  }, []);
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
